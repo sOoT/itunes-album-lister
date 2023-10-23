baa73d75d2e2f4fac4fb2d9b9397fb4820b81c85
Ismet Yavuz <info@ismetyavuz.com>
2023-10-23 19:33:20 +0200

    init

---
+++ src/app/albums/album-list/album-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-list',
+  templateUrl: './album-list.component.html',
+  styleUrls: ['./album-list.component.scss']
+})
+export class AlbumListComponent {
+
+}
