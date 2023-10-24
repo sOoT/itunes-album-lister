88188c251acf44b4c43fd5b7970e4b5402807be0
Ismet Yavuz <info@ismetyavuz.com>
2023-10-24 21:05:11 +0200

    adjust header styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AlbumsComponent } from './albums/albums.component';
-import { AlbumListComponent } from './albums/album-list/album-list.component';
-import { AlbumDetailComponent } from './albums/album-detail/album-detail.component';
-import { SearchComponent } from './search/search.component';
-import { AlbumComponent } from './albums/album-list/album/album.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { AlbumsComponent } from './modules/albums/albums.component';
+import { AlbumListComponent } from './modules/albums/album-list/album-list.component';
+import { AlbumDetailComponent } from './modules/albums/album-detail/album-detail.component';
+import { SearchComponent } from './modules/header/search/search.component';
+import { AlbumComponent } from './modules/albums/album-list/album/album.component';
 
 @NgModule({
   declarations: [
